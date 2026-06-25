f37b7b6df1e6d517938c27f4952a100fb463ccc2
yuluoqc <1173362798@qq.com>
2026-06-25 16:03:34 +0800

    修正天气问题

--- src/views/Func/index.vue
+++ src/views/Func/index.vue
@@ -21,7 +21,6 @@
               <span> {{ currentTime.hour }}:{{ currentTime.minute }}:{{ currentTime.second }}</span>
             </div>
           </div>
-          <Weather />
         </div>
       </el-col>
     </el-row>
@@ -33,7 +32,6 @@ import { getCurrentTime } from "@/utils/getTime";
 import { mainStore } from "@/store";
 import Music from "@/components/Music.vue";
 import Hitokoto from "@/components/Hitokoto.vue";
-import Weather from "@/components/Weather.vue";
 
 const store = mainStore();
 
@@ -111,7 +109,7 @@ onBeforeUnmount(() => {
       display: flex;
       flex-direction: column;
       align-items: center;
-      justify-content: space-between;
+      justify-content: center;
       animation: fade 0.5s;
       .time {
         font-size: 1.1rem;
@@ -127,13 +125,6 @@ onBeforeUnmount(() => {
           letter-spacing: 2px;
           font-family: "UnidreamLED";
         }
-      }
-      .weather {
-        text-align: center;
-        width: 100%;
-        text-overflow: ellipsis;
-        overflow-x: hidden;
-        white-space: nowrap;
       }
     }
   }
